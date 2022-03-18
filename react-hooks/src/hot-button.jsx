d12a33982bf988b7826c05f16268b09cde49e1cb
vkahlon <vickram.kahlon@gmail.com>
2022-03-17 19:19:55 -0700

    Set-up enviorment

---
+++ react-hooks/src/hot-button.jsx
@@ -0,0 +1,7 @@
+import React. {useState} from 'react';
+export default function HotButton(intialItems) {
+  const [items, setItems] = useState(intialItems)
+    return (
+      <h2>I am Hot Button</h2>
+    )
+}
